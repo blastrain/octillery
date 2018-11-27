9c6b9e56b9f999b5cc2ed4905ab096dbd97c9ad2
Masaaki Goshima <goccy54@gmail.com>
2018-11-27 20:48:57 +0900

    fix internal package by golint

--- internal/plugin.go
+++ internal/plugin.go
@@ -2,10 +2,12 @@ package internal
 
 var loadedPlugin = map[string]bool{}
 
+// IsLoadedPlugin used by plugin and connectin/adapter/plugin package to escape duplicated loading.
 func IsLoadedPlugin(plugin string) bool {
 	return loadedPlugin[plugin]
 }
 
+// SetLoadedPlugin used by plugin and connectin/adapter/plugin package to escape duplicated loading.
 func SetLoadedPlugin(plugin string) {
 	loadedPlugin[plugin] = true
 }
