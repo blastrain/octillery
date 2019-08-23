c11488020f363b9cbbbd9f20c77e32334752766f
TatsuNet <yoichi.tatsuzumi@knocknote.co.jp>
2019-08-23 18:59:07 +0900

    Returned to two cases, becase must use type assertion.

--- sqlparser/sqlparser.go
+++ sqlparser/sqlparser.go
@@ -224,19 +224,18 @@ func (p *Parser) replaceInsertValueFromValArg(query *InsertQuery, colIndex int, 
 				Val:  []byte(fmt.Sprintf("%d", arg)),
 			}
 		}
-	case time.Time, *time.Time:
+	case time.Time:
 		query.ColumnValues[colIndex] = func() *vtparser.SQLVal {
-			var strTime string
-			layout := "2006-01-02 15:04:05"
-			if t, ok := arg.(time.Time); ok {
-				strTime = t.Format(layout)
-			}
-			if t, ok := arg.(*time.Time); ok {
-				strTime = t.Format(layout)
-			}
 			return &vtparser.SQLVal{
 				Type: vtparser.StrVal,
-				Val:  []byte(strTime),
+				Val:  []byte(arg.Format("2006-01-02 15:04:05")),
+			}
+		}
+	case *time.Time:
+		query.ColumnValues[colIndex] = func() *vtparser.SQLVal {
+			return &vtparser.SQLVal{
+				Type: vtparser.StrVal,
+				Val:  []byte(arg.Format("2006-01-02 15:04:05")),
 			}
 		}
 	case nil:
